3428f4e9c0ba359114175a96cc534f2c6f734b9c
Dmitry1232321 <cuznetsov.dmitri2003@yandex.ru>
2025-01-09 00:44:07 +0300

    add console branch

---
+++ RocketWanderer/Console/Views/Game/CircleDrawer.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp.Views.Game.PlanetViewConsole;
+
+namespace ConsoleApp.Views.Game
+{
+  /// <summary>
+  /// Художник круга
+  /// </summary>
+  public class CircleDrawer
+  {
+    /// <summary>
+    /// Делегат, который проверяет подходящая ли дистанция
+    /// </summary>
+    /// <param name="distance">Дистанция</param>
+    /// <returns>true - переданная дистанция успешно прошла проверку, иначе false</returns>
+    public delegate bool dDistanceChecker(double distance);
+
+    /// <summary>
+    /// Рисует круг в консоли
+    /// </summary>
+    /// <param name="parRadiusX">Радиус круга по X</param>
+    /// <param name="parRadiusY">Радиус круга по Y</param>
+    /// <param name="parXOffset">Смещение позиции по X</param>
+    /// <param name="parYOffset">Смещение позиции по Y</param>
+    /// <param name="parFillChar">Символ для заполнения</param>
+    /// <param name="parDistanceChecker">Условие для отрисовки символа</param>
+    public static void Draw(int parRadiusX, int parRadiusY,
+          double parXOffset, double parYOffset,
+          char parFillChar, dDistanceChecker parDistanceChecker)
+    {
+      ConsoleAdapter console = ConsoleAdapter.Instance;
+
+      for (int y = -parRadiusY; y <= parRadiusY; y++)
+      {
+        for (int x = -parRadiusX; x <= parRadiusX; x++)
+        {
+          double normalizedX = (double)x / parRadiusX;
+          double normalizedY = (double)y / parRadiusY;
+
+          double distance = Math.Sqrt(normalizedX * normalizedX + normalizedY * normalizedY);
+
+          if (parDistanceChecker(distance))
+          {
+            double xPos = x + parXOffset;
+            double yPos = y + parYOffset;
+
+            console.WriteBuffer((int)xPos, (int)yPos, parFillChar);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ RocketWanderer/Console/Views/Game/SunViewConsole.cs
@@ -0,0 +1,80 @@
+﻿using ConsoleApp.App;
+using Logic.Models.Game;
+using Logic.Utils;
+using Logic.Views.Game;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp.Views.Game.PlanetViewConsole;
+
+namespace ConsoleApp.Views.Game
+{
+  /// <summary>
+  /// Представление солнца от Console
+  /// </summary>
+  public class SunViewConsole : SunView
+  {
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="parSun">Модель солнца</param>
+    public SunViewConsole(Sun parSun) 
+      : base(parSun)
+    {
+    }
+
+    /// <summary>
+    /// Прямоугольник, который был отрисован последний раз
+    /// </summary>
+    private Rect prevLocation = new Rect();
+
+    /// <summary>
+    /// Прямоугольник, который был отрисован последний раз
+    /// </summary>
+    public Rect PrevLocation
+    {
+      get { return prevLocation; }
+    }
+
+    /// <summary>
+    /// Отрисовывает солнце
+    /// </summary>
+    public override void Draw()
+    {
+      base.Draw();
+
+      Vector2 parentSize = Parent.AbsoluteSize;
+      
+      ConsoleAdapter console = ConsoleAdapter.Instance;
+
+      if (Sun.Position.X + Sun.Size.X / 2 > Map.XCameraOffset)
+      {
+        double scaleY = parentSize.Y / Map.Size.Y;
+        double scaleX = scaleY * FontInfo.FontYScale;
+
+        double width = Sun.Size.X * scaleX;
+        double height = Sun.Size.Y * scaleY;
+
+        double xPosOffset = (Sun.Position.X - Map.XCameraOffset) * scaleX;
+        double yPosOffset = Sun.Position.Y * scaleY;
+
+        int radiusX = (int)(width / 2);
+        int radiusY = (int)(height / 2);
+        char fillChar = '@';
+        
+        prevLocation = new Rect((int)xPosOffset - radiusX, (int)yPosOffset - radiusY, (int)width, (int)height);
+
+        CircleDrawer.Draw(radiusX, radiusY, xPosOffset, yPosOffset, fillChar,
+          (distance) =>
+          {
+            return distance <= 1;
+          });
+      }
+    }
+
+    
+  }
+}
